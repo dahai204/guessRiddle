2a75f41cccfff28a1c89995d4620539fa408b451
dahai <dahai204@163.com>
2020-05-11 15:59:35 +0800

    Create readme.mk
    Add readme file

---
+++ readme.mk
@@ -0,0 +1,8 @@
+# readme.mk
+This is python project for riddle.
+
+The file 'riddleMessage.txt' is the riddles for the game,the user can change it before strating the guess game.
+
+The file 'riddleMessage.py' is reading riddles from the txt files.
+
+
